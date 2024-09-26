2a05e1834ea1f0fc90af8610c86e18e763d30558
Toey8ES <toeyyb0829@gmail.com>
2024-09-27 00:02:14 +0700

    T

---
+++ api-ToeyES.js
@@ -0,0 +1,16 @@
+function updateData() {
+  // แทนที่ด้วยโค้ดสำหรับดึงข้อมูลของคุณ
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    // document.getElementById('timestamp').textContent = data.timestamp;
+    document.getElementById('timestamp').textContent = data.lat;
+    document.getElementById('temperature').textContent = data.temperature.toFixed(2);
+    document.getElementById('humidity').textContent = data.humidity.toFixed(2);
+    document.getElementById('luxsensor').textContent = data.luxsensor.toFixed(2);
+    document.getElementById('motion').textContent = data.motion;
+  });
+}
+setInterval(updateData, 1000); // เรียกใช้ฟังก์ชัน updateData ทุก 1000 มิลลิวินาที (1 วินาที)
+
+// <script src="get.js"></script> วิธีเรียกหา js
